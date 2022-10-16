76f1532f292aef2d2c6338779d5698ef4df272f7
Ekaterina Frolova <ekaterina_frolova1@epam.com>
2022-10-16 17:51:16 +0300

    hw3 is completed

---
+++ Lection3_HW/hw3.py
@@ -0,0 +1,44 @@
+# https://www.python.org/dev/peps/pep-0570/#logical-ordering
+# Positional-only parameters also have the (minor) benefit of enforcing some logical order when
+# calling interfaces that make use of them. For example, the range function takes all its
+# parameters positionally and disallows forms like:
+
+# range(stop=5, start=0, step=2)
+# range(stop=5, step=2, start=0)
+# range(step=2, start=0, stop=5)
+# range(step=2, stop=5, start=0)
+
+# at the price of disallowing the use of keyword arguments for the (unique) intended order:
+
+# range(start=0, stop=5, step=2)
+"""
+Write a function that accept any sequence (list, string, tuple) of unique values and then
+it behaves as range function:
+
+
+import string
+
+
+assert = custom_range(string.ascii_lowercase, 'g') == ['a', 'b', 'c', 'd', 'e', 'f']
+assert = custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
+assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', 'j', 'h']
+
+"""
+from typing import Sequence
+import string
+
+
+def custom_range(seq: Sequence, stop_, start=0, step=1) -> list:
+    result = []
+
+    if type(start) == str and type(stop_) == str:
+        result = list(seq[seq.index(stop_):seq.index(start):step])
+    elif type(start) == int and type(stop_) == int:
+        result = list(seq[start:stop_:step])
+    elif type(start) == str and type(stop_) == int:
+        result = list(seq[seq.index(start):stop_:step])
+    elif type(start) == int and type(stop_) == str:
+        result = list(seq[start:seq.index(stop_):step])
+
+    return result
+
